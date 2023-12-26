7de8724bcdce91b415ab902201dccb18a60d486e
JEAN LOUIS DOS SANTOS <71648706+jeanlouisdossantos@users.noreply.github.com>
2023-12-26 14:51:53 +0000

    truc

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-msg = input("entrer votre messages \n")
-for letter in msg:
-    print(letter)
+# msg = input("entrer votre messages \n")
+# for letter in msg:
+#     print(letter)
+
+message = {1:"test",2:"test2"}
+for items in message.items():
+    for truc in items:
+        print(truc)
