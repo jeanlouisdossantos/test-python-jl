eff8ecf24d8a95483a6bd0cef3a2035aa4d87f44
JEAN LOUIS DOS SANTOS <71648706+jeanlouisdossantos@users.noreply.github.com>
2023-12-29 15:00:09 +0000

    maj test;py

--- test.py
+++ test.py
@@ -1,8 +1,23 @@
 # msg = input("entrer votre messages \n")
 # for letter in msg:
 #     print(letter)
+def test ():
+    message = {1:"test",2:"test2"}
+    for items in message.items():
+        for truc in items:
+            print(truc)
+def fonction_test ():
+    pass
 
-message = {1:"test",2:"test2"}
-for items in message.items():
-    for truc in items:
-        print(truc)
+
+def main ():
+    while True:
+        range_size = int(input("\nEnter un nombre\n"))
+        new_list = [x for x in range(range_size) if x%3==0]
+        print (new_list)
+
+
+
+
+if __name__=="__main__":
+    main()
